c3c850d3de7b885233d54e7cbb016f72c9fdea88
Max Chuquimia <max.chuquimia@users.noreply.github.io>
2021-04-07 20:26:06 +1000

    Use an open class instead of a protocol for XCGrapherPlugin

--- Sources/XCGrapherPluginSupport/XCGrapherPlugin.swift
+++ Sources/XCGrapherPluginSupport/XCGrapherPlugin.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 /// An interface of a dynamically loadable `xcgrapher` plugin
-public protocol XCGrapherPlugin: AnyObject {
+open class XCGrapherPlugin {
 
     // The objects produced by the processing functions can be anything and will be returned in the makeEdges function.
     // Using a generic type here overcomplicates plugin loading and doesn't allow use of multiple types.
@@ -10,13 +10,19 @@ public protocol XCGrapherPlugin: AnyObject {
     // associatedtype Any
 
     /// Produces a list of graph nodes based on the contents of a source file.
-    func process(file: XCGrapherFile) throws -> [Any]
+    func process(file: XCGrapherFile) throws -> [Any] {
+        []
+    }
 
     /// Produces a list of graph nodes based on an `import X` line.
-    func process(library: XCGrapherImport) throws -> [Any]
+    func process(library: XCGrapherImport) throws -> [Any] {
+        []
+    }
 
     /// Produces a list of `XCGrapherEdge` (arrows) from the results of `process(file:)` and `process(library:)`.
     /// Duplicate `XCGrapherEdge`s will automatically be discarded.
-    func makeEdges(from nodes: [Any]) throws -> [XCGrapherEdge]
+    func makeEdges(from nodes: [Any]) throws -> [XCGrapherEdge] {
+        []
+    }
 
 }
